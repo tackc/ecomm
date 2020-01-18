a86359b2717ecb509cd21630e3c44a3c332ca8ca
Chris Tack <chris_tack@mac.com>
2020-01-17 17:23:43 -0800

    Return attrs after creating account

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
 // By adding this here, you can avoid having to put the entire code snippet into each route handler. i.e. app.post('/', bodyParser.urlencoded({ extended: true }) ,(req, res) => {...
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['lkjsdMEIlk3845lkjfsLJD']
+}))
 
 //Route handler (what to do when a request comes in from the browser)
 app.get('/', (req, res) => {
@@ -34,6 +38,12 @@ app.post('/', async (req, res) => {
     if (password !== passwordConfirmation) {
         return res.send('Passwords must match');
     }
+
+    // Create user in user repo to represent this person
+    const user = await usersRepo.create({ email, password });
+
+    // Store the id of that user inside users cookies
+    req.session.userId = user.id;
 
     res.send('Account created!!!');
 });
