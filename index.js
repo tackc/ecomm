747a7266f69be222c0ab64e0956bae0c09bef348
Chris Tack <chris_tack@mac.com>
2020-01-19 14:08:53 -0800

    Create /signin route

--- index.js
+++ index.js
@@ -55,6 +55,36 @@ app.get('/signout', (req, res) => {
     res.send('You are logged out!')
 })
 
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+    `)
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+        return res.send('Email not found');
+    }
+
+    if (user.password !== password) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!!');
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
