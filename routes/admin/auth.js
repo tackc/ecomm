17c825d4e9d3b5f19c152ba5bcf2565c60c51eb4
Chris Tack <chris_tack@mac.com>
2020-01-26 20:58:19 -0800

    Update password validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -58,24 +58,30 @@ router.post('/signin', [
         }),
     check('password')
         .trim()
+        .custom(async (password, { req }) => {
+            const user = await usersRepo.getOneBy({ email: req.body.email })
+                if (!user) {
+                    throw new Error('Invalid password');
+                }
+
+            const validPassword = await usersRepo.comparePasswords(
+                user.password,
+                password
+            );
+            if (!validPassword) {
+                throw new Error('Invalid password');
+            }
+        })
 ], async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
-    
+
     const { email, password } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
 
     if (!user) {
         return res.send('Email not found');
-    }
-
-    const validPassword = await usersRepo.comparePasswords(
-        user.password,
-        password
-    );
-    if (!validPassword) {
-        return res.send('Invalid password');
     }
 
     req.session.userId = user.id;
