948a23c34fe599fc87787b027562adce3640f219
Chris Tack <chris_tack@mac.com>
2020-02-08 22:02:02 -0800

    Move reuseable methods into parent class called repository

---
+++ repositories/repository.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+module.exports = class Repository {
+    //constructor functions get called immediately whenever we create a new instance of a class
+    //constructor functions are not allowed to be async in nature...that's why we are using fs.accessSync...normally not used much because there is no callback (it's not asynchronous)
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('Creating a repository requires a filename');
+        }
+    
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+
+    async create(attrs) {
+        attrs.id = this.randomId();
+
+        const records = await this.getAll();
+        records.push(attrs);
+        await this.writeAll(recorts);
+
+        return attrs
+    }
+    
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+            encoding: 'utf8'
+            })
+        );
+    }
+
+    async writeAll(records) {
+        // write the updated records array to this.filename
+        await fs.promises.writeFile(
+            this.filename, 
+            JSON.stringify(records, null, 2)
+        );
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        //takes the info in the second argument and adds it to the first argument
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+        
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            if (found) {
+                return record;
+            }
+        }
+    }
+}
--- repositories/users.js
+++ repositories/users.js
@@ -1,33 +1,11 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require('./repository');
 
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
-    //constructor functions get called immediately whenever we create a new instance of a class
-    //constructor functions are not allowed to be async in nature...that's why we are using fs.accessSync...normally not used much because there is no callback (it's not asynchronous)
-    constructor(filename) {
-        if (!filename) {
-            throw new Error('Creating a repository requires a filename');
-        }
-    
-        this.filename = filename;
-        try {
-            fs.accessSync(this.filename);
-        } catch (err) {
-            fs.writeFileSync(this.filename, '[]');
-        }
-    }
-    
-    async getAll() {
-        return JSON.parse(
-            await fs.promises.readFile(this.filename, {
-            encoding: 'utf8'
-            })
-        );
-    }
-
+class UsersRepository extends Repository {
     async create(attrs) {
         // attrs = { email: '', password: '' }
         attrs.id = this.randomId();
@@ -58,59 +36,6 @@ class UsersRepository {
 
         // Remember this is a buffer
         return hashed === hashedSuppliedBuf.toString('hex');
-    }
-
-    async writeAll(records) {
-        // write the updated records array to this.filename
-        await fs.promises.writeFile(
-            this.filename, 
-            JSON.stringify(records, null, 2)
-        );
-    }
-
-    randomId() {
-        return crypto.randomBytes(4).toString('hex');
-    }
-
-    async getOne(id) {
-        const records = await this.getAll();
-        return records.find(record => record.id === id);
-    }
-
-    async delete(id) {
-        const records = await this.getAll();
-        const filteredRecords = records.filter(record => record.id !== id);
-        await this.writeAll(filteredRecords);
-    }
-
-    async update(id, attrs) {
-        const records = await this.getAll();
-        const record = records.find(record => record.id === id);
-
-        if (!record) {
-            throw new Error(`Record with id ${id} not found`);
-        }
-
-        //takes the info in the second argument and adds it to the first argument
-        Object.assign(record, attrs);
-        await this.writeAll(records);
-    }
-
-    async getOneBy(filters) {
-        const records = await this.getAll();
-        
-        for (let record of records) {
-            let found = true;
-
-            for (let key in filters) {
-                if (record[key] !== filters[key]) {
-                    found = false;
-                }
-            }
-            if (found) {
-                return record;
-            }
-        }
     }
 }
 
