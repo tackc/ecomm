a0a5b4ae52b46929b4849e9f37afd97d1157c192
Chris Tack <chris_tack@mac.com>
2020-02-21 08:09:52 -0800

    Create route handler for edit product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,6 +6,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -35,6 +36,17 @@ router.post('/admin/products/new',
 
     // Send user to route after successfully creating new product
     res.redirect('/admin/products');
+});
+
+// :id is known as a wildcard. It is used here to pass an id of a product
+router.get('/admin/products/:id/edit', async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found');
+    }
+
+    res.send(productsEditTemplate({ product }));
 });
 
 module.exports = router;
