4afd21114a5c3bc37ae1f77ce39a3524f5c7f8db
Chris Tack <chris_tack@mac.com>
2020-03-02 14:28:20 -0800

    Create cart route

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -34,6 +36,21 @@ router.post('/cart/products', async (req, res) => {
 })
 
 // Receive a GET request to show all items in cart
+router.get('/carts', async (req, res) => {
+    if (!req.session.cartId) {
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId)
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+})
 
 // Receive a POST request to delete an item from a cart
 
