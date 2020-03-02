5996e3f6ce1715794238a45f0865c42dbdff540a
Chris Tack <chris_tack@mac.com>
2020-03-02 14:44:53 -0800

    Fix formatting and add item id to form submission

--- views/carts/show.js
+++ views/carts/show.js
@@ -17,19 +17,20 @@ module.exports = ({ items }) => {
                 <h3 class="subtitle">${item.product.title}</h3>
                 <div class="cart-right">
                     <div>
-                    $${item.product.price}  X  ${item.quantity} = 
+                        $${item.product.price}  X  ${item.quantity} = 
                     </div>
                     <div class="price is-size-4">
-                    $${item.product.price * item.quantity}
+                        $${item.product.price * item.quantity}
                     </div>
                     <div class="remove">
-                    <form method="POST">
-                        <button class="button is-danger">                  
-                        <span class="icon is-small">
-                            <i class="fas fa-times"></i>
-                        </span>
-                        </button>
-                    </form>
+                        <form method="POST" action"/cart/products/delete">
+                        <input hidden value="${item.id}" name="itemId" />
+                            <button class="button is-danger">                  
+                                <span class="icon is-small">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
