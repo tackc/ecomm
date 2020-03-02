691d9da70ee8f12d7b44d4a63b23a3ced8390d0e
Chris Tack <chris_tack@mac.com>
2020-03-02 14:07:10 -0800

    Create cart if one does not already exist

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 // Receive a POST request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-    console.log(req.body.productId)
+router.post('/cart/products', async (req, res) => {
+    // Figure out the cart
+    let cart;
+    if (!req.session.cartId) {
+        // We don't have a cart and need to create one. Store the cart id on the req.session.cartId property
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        // We have a cart! Let's get it from the repository
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    console.log(cart);
+
+    // Either increment quantity for existing product or add new product into items array
+
+
+
     res.send('Product added to cart')
 })
 
